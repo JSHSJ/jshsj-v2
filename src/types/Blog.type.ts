edfc0a67f3536f48011f744d9567908c3b4a3940
Joshua <joshua.stuebner@bloxxter.com>
2022-04-15 13:09:00 +0200

    feat: add types

---
+++ src/types/Blog.type.ts
@@ -0,0 +1,7 @@
+export type BlogPost = {
+  title: string;
+  description: string;
+  tags: string[];
+  date: string;
+  lang: string;
+}
---
+++ src/types/Project.type.ts
@@ -0,0 +1,8 @@
+export type Project  = {
+  title: string;
+  description: string;
+  link: string;
+  techstack: string;
+  year: number;
+  date: string;
+}
